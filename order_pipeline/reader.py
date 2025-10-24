cdc2a239153e83c626dbedd07bf5a1b90f5a422a
Ekenedilichukwu Agwu <agwuekene@gmail.com>
2025-10-24 11:40:01 +0100

    Update reader.py

--- order_pipeline/reader.py
+++ order_pipeline/reader.py
@@ -1,15 +1,19 @@
 import json
+import re
 from typing import Any
 
 class Reader:
    
-    def read(self) -> list[dict[str, Any]]:
-        try:
-            with open('shoplink.json', 'r') as file:
-                content: list[dict[str, Any]] = json.load(file)
-                return content
-        except json.JSONDecodeError:
-            raise ValueError('File is empty or invalid')
+    def read(self, path: str) -> list[dict[str, Any]]:
+        if re.search(r'\.json$', path):
+            try:
+                with open(path, 'r') as file:
+                    content: list[dict[str, Any]] = json.load(file)
+                    return content
+            except json.JSONDecodeError:
+                raise ValueError('File is empty or invalid')
+        else:
+            raise ValueError('Expected a JSON file')
+    
 
-
-#print(Reader().read('shoplink.json')[:2])           
+print(Reader().read('shoplink.json')[:2])           
